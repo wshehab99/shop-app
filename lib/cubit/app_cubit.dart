b1b2b23093764db1b35e2becd49108d4e0dc43c9
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-08-12 06:30:12 +0200

    login screen cubit

--- lib/cubit/app_cubit.dart
+++ lib/cubit/app_cubit.dart
@@ -1,6 +1,32 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/cubit/app_states.dart';
+import 'package:shop_app/shared/network/dio_helper.dart';
 
 class AppCubit extends Cubit<AppStates> {
-  AppCubit() : super(InitialAppState());
+  bool showPassword = true;
+  AppCubit(super.initialState);
+  void login({
+    required String email,
+    required String password,
+  }) {
+    emit(LoadingState());
+    DioHelper.postData(
+      url: "login",
+      data: {
+        'email': email,
+        "password": password,
+      },
+    ).then((value) {
+      emit(LoginSuccessState());
+      print(value.data);
+    }).catchError((onError) {
+      print(onError);
+      emit(LoginErrorState(error: onError.toString()));
+    });
+  }
+
+  void passwordPressed() {
+    showPassword = !showPassword;
+    emit(ChangePasswordVisablityState());
+  }
 }
--- lib/cubit/app_states.dart
+++ lib/cubit/app_states.dart
@@ -1,3 +1,14 @@
 abstract class AppStates {}
 
 class InitialAppState extends AppStates {}
+
+class LoadingState extends AppStates {}
+
+class LoginSuccessState extends AppStates {}
+
+class LoginErrorState extends AppStates {
+  String error;
+  LoginErrorState({required this.error});
+}
+
+class ChangePasswordVisablityState extends AppStates {}
